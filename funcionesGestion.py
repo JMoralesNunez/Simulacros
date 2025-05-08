cf2e9f6e1a5e3dce58d886888fa50ab5c8df26af
JMoralesNunez <136384939+JMoralesNunez@users.noreply.github.com>
2025-05-07 21:38:45 -0500

    Update funcionesGestion.py

--- funcionesGestion.py
+++ funcionesGestion.py
@@ -4,14 +4,30 @@ def add(id,name,age,grade, dict):
     print(f"{name}, with id: {id}, has been successfully added!")
 
 def search_id(id, dict):
-    return dict.get(id)
+    format=dict.get(id)
+    if format == None:
+        print("Estudiante no encontrado")
+    else:
+        print("Nombre Completo: {fullname}, Edad: {age}, Nota: {grade}".format_map(format))
     
 def search_name(name,dict):
     for k,v in dict.items():
         if name == v["fullname"]:
             print("Estudiante encontrado:")
-            print(f"{k}....... Nombre completo: {v["fullname"]} Edad: {v["age"]} Nota: {v["grade"]}")
+            print(f"ID: {k}....... Nombre completo: {v["fullname"]} Edad: {v["age"]} Nota: {v["grade"]}")
             break
         else:
             print("Estudiante no encontrado")
             break
+
+def update_age(id, age, dict):
+    dict[id]["age"]=age
+    format=dict.get(id)
+    print("Cambio realizado exitosamente!")
+    print("Nombre Completo: {fullname}, Edad: {age}, Nota: {grade}".format_map(format))
+
+def update_grade(id, grade, dict):
+    dict[id]["grade"]=grade
+    format=dict.get(id)
+    print("Cambio realizado exitosamente!")
+    print("Nombre Completo: {fullname}, Edad: {age}, Nota: {grade}".format_map(format))
